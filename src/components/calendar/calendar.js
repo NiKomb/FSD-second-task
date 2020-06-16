5e77a9db258fe1baaaff1952fbf77c7bdf7a12a7
Nickolai Kombin <5362798@gmail.com>
2020-06-16 20:06:29 +0500

    add showing calendar on field click

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,16 +1,18 @@
-import "air-datepicker/dist/js/datepicker"
-import "air-datepicker/dist/css/datepicker.min.css"
+import "air-datepicker/dist/js/datepicker";
+import "air-datepicker/dist/css/datepicker.min.css";
 
-import "./calendar.scss"
+import "./calendar.scss";
 
 class Calendar {
-  constructor(combine, options) {
-    this.combine = combine;
+  constructor(calendarField, options) {
+    this.calendarField = calendarField;
 
     this.initDatePicker(options);
-    this.createButtons();
+    this.changeButtonsNames();
 
-    this.calendar = $(combine).data('datepicker').$datepicker;
+    this.addEventListeners();
+
+    this.calendar = $(calendarField).data("datepicker").$datepicker;
 
     this.calendar.hide();
   }
@@ -18,16 +20,45 @@ class Calendar {
   initDatePicker(options) {
     const defaultOptions = {
       classes: options.className,
+      inline: false,
       range: true,
       minDate: new Date(),
       clearButton: true,
       todayButton: true,
     };
 
-    $(this.combine).datepicker({ ...defaultOptions, ...options });
+    $(this.calendarField).datepicker({ ...defaultOptions, ...options });
   }
 
+  changeButtonsNames() {
+    const datePickerButtons = $(this.calendarField)
+      .data("datepicker")
+      .$datepicker[0].querySelector(".datepicker--buttons");
 
+    datePickerButtons.innerHTML = "";
+    const clearButtonWrapper = `<span class="datepicker--button" data-action="clear">Очистить</span>`;
+    const applyButtonWrapper = `<span class="datepicker--button" data-action="today">Применить</span>`;
+
+    datePickerButtons.insertAdjacentHTML(
+      "beforeend",
+      [clearButtonWrapper, applyButtonWrapper].join("")
+    );
+  }
+
+  addEventListeners() {
+    $(this.calendarField).on("click", this.showCalendar.bind(this));
+    $('span[data-action="today"]').on("click", this.hideCalendar.bind(this));
+    $(document.body).on("mouseup", this.hideCalendar.bind(this));
+  }
+
+  showCalendar(event) {
+    this.calendar.show();
+  }
+
+  hideCalendar(event) {
+    event.stopPropagation();
+    this.calendar.hide();
+  }
 }
 
 export default Calendar;
