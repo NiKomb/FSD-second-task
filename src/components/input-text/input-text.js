1b0c1670360a590064a354a555705ee06874c707
Nickolai Kombin <5362798@gmail.com>
2020-06-18 20:57:02 +0500

    add second type of calendar field

--- src/components/input-text/input-text.js
+++ src/components/input-text/input-text.js
@@ -1,1 +1,23 @@
-import './input-text'
+import Calendar from "../../components/calendar/calendar";
+
+const arrivalCalendar = new Calendar(".input_view-range_calendar", {
+  className: "js-input_view-range__arrival-calendar",
+  multipleDatesSeparator: "-",
+  onSelect(formattedDate) {
+    $(".input_view-range_arrival .input__field").val(
+      formattedDate.split("-")[0]
+    );
+    $(".input_view-range_check-out .input__field").val(
+      formattedDate.split("-")[1]
+    );
+  },
+});
+
+const filterCalendar = new Calendar(
+  ".input_with-filter-date-calendar .field-input",
+  {
+    className: ".js-input_with-filter-date-calendar",
+    dateFormat: "dd M",
+    multipleDatesSeparator: " - ",
+  }
+);
