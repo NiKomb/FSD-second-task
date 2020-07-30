e3bec1dc70cfb8cf7c66773ca7156f01ec890bd1
Nickolai Kombin <5362798@gmail.com>
2020-07-30 18:10:54 +0500

    fix ion-rangeslider`s styles and bars

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -2,18 +2,29 @@ import "ion-rangeslider";
 import "ion-rangeslider/css/ion.rangeSlider.css";
 import "./range-slider";
 
-$(document).ready(function () {
-  const $slider = $(".slider__item_with-range-slider");
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const slider = $(".slider__item_with-range-slider");
+    const sliderTitle = document.querySelector(".slider__title");
 
-  $slider.ionRangeSlider({
-    type: "double",
-    // min: 0,
-    // max: 15000,
-    // from: 5000,
-    // to: 10000,
-    hide_min_max: true,
-    // hide_from_to: true,
-    values_separator: " - ",
-    postfix: "₽",
-  });
-});
+    slider.ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 15000,
+      from: 5000,
+      to: 10000,
+      hide_min_max: true,
+      hide_from_to: true,
+      values_separator: " - ",
+      postfix: "₽",
+      onStart(data) {
+        sliderTitle.dataset.subtitle = `${data.from}₽ - ${data.to}₽`;
+      },
+      onChange(data) {
+        sliderTitle.dataset.subtitle = `${data.from}₽ - ${data.to}₽`;
+      },
+    });
+  },
+  { once: true }
+);
