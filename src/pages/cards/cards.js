cc06c0ea1e0069b30a99d63395055f4604ee9241
Nickolai Kombin <5362798@gmail.com>
2020-08-10 19:54:30 +0500

    add room card with hover`s view like slider

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,1 +1,14 @@
-import './cards.scss'
+import "./cards.scss";
+
+import "@components/input-text/input-text";
+
+import Calendar from "../../components/calendar/calendar";
+
+const cardsCalendar = new Calendar(
+  ".cards__item_with-calendar",
+  {
+    className: "js-cards__calendar",
+  },
+  true
+);
+cardsCalendar.showCalendar();
