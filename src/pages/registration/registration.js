807d32af5c112dc559a6a4bece6118c1c9da590c
Nickolai Kombin <5362798@gmail.com>
2020-08-14 20:36:21 +0500

    add registration page

---
+++ src/pages/registration/registration.js
@@ -0,0 +1,7 @@
+function requireAll(r) {
+  return r.keys().map(r);
+}
+
+requireAll(require.context("@components", true, /\.js|scss$/));
+requireAll(require.context("@/pages", true, /\.js|scss$/));
+// requireAll(require.context("`@/theme", true, /\.js|scss$/));
