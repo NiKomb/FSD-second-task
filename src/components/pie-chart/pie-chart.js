24c208743b6abf5c51a5e2f1aae02e0a4fcbb923
Nickolai Kombin <5362798@gmail.com>
2020-08-20 17:11:44 +0500

    fix animation for pie-chart, new gradients in vars

--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,15 +1,28 @@
-const legendList = document.querySelectorAll(".js-pie-chart__item");
+const legendList = document.querySelectorAll("li.js-pie-chart__item");
 const circlesList = document.querySelectorAll(".js-pie-chart__circle");
-
-console.log(legendList);
-console.log(circlesList);
+const votesList = document.querySelectorAll(".js-pie-chart__votes");
+const total = document.querySelector(".js-pie-chart__total");
 
 legendList.forEach(function (item, index) {
   item.addEventListener("mouseover", function () {
     circlesList[index].classList.add("pie-chart__circle_hovered");
+    total.classList.add("pie-chart__total_invisible");
   });
 
   item.addEventListener("mouseout", function () {
     circlesList[index].classList.remove("pie-chart__circle_hovered");
+    total.classList.remove("pie-chart__total_invisible");
+  });
+});
+
+circlesList.forEach(function (item, index) {
+  item.addEventListener("mouseover", function () {
+    votesList[index].classList.add("pie-chart__votes_visible");
+    total.classList.add("pie-chart__total_invisible");
+  });
+
+  item.addEventListener("mouseout", function () {
+    votesList[index].classList.remove("pie-chart__votes_visible");
+    total.classList.remove("pie-chart__total_invisible");
   });
 });
