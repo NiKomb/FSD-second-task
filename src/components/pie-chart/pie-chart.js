b085c45b138c8dae77e32f233005e35f9aeed3b2
Nickolai Kombin <5362798@gmail.com>
2020-08-24 21:18:25 +0500

    header`s style fix

--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,28 +1,35 @@
-const legendList = document.querySelectorAll("li.js-pie-chart__item");
+const itemList = document.querySelectorAll("li.js-pie-chart__item");
 const circlesList = document.querySelectorAll(".js-pie-chart__circle");
 const votesList = document.querySelectorAll(".js-pie-chart__votes");
 const total = document.querySelector(".js-pie-chart__total");
 
-legendList.forEach(function (item, index) {
-  item.addEventListener("mouseover", function () {
-    circlesList[index].classList.add("pie-chart__circle_hovered");
-    total.classList.add("pie-chart__total_invisible");
-  });
+itemList.forEach(function (item, i) {
+  item.addEventListener("mouseover", handleItemMouseOver);
+  item.addEventListener("mouseout", handleItemMouseOut);
 
-  item.addEventListener("mouseout", function () {
-    circlesList[index].classList.remove("pie-chart__circle_hovered");
+  function handleItemMouseOver() {
+    circlesList[i].classList.add("pie-chart__circle_hovered");
+
+    total.classList.add("pie-chart__total_invisible");
+  }
+
+  function handleItemMouseOut() {
+    circlesList[i].classList.remove("pie-chart__circle_hovered");
     total.classList.remove("pie-chart__total_invisible");
-  });
+  }
 });
 
-circlesList.forEach(function (item, index) {
-  item.addEventListener("mouseover", function () {
-    votesList[index].classList.add("pie-chart__votes_visible");
-    total.classList.add("pie-chart__total_invisible");
-  });
+circlesList.forEach(function (circle, i) {
+  circle.addEventListener("mouseover", handleCircleMouseOver);
+  circle.addEventListener("mouseout", handleCircleMouseOut);
 
-  item.addEventListener("mouseout", function () {
-    votesList[index].classList.remove("pie-chart__votes_visible");
+  function handleCircleMouseOver() {
+    votesList[i].classList.add("pie-chart__votes_visible");
+    total.classList.add("pie-chart__total_invisible");
+  }
+
+  function handleCircleMouseOut() {
+    votesList[i].classList.remove("pie-chart__votes_visible");
     total.classList.remove("pie-chart__total_invisible");
-  });
+  }
 });
